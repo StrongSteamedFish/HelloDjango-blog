53eebac2d73668d54314e98a9652ffb3d1454cb6
SteamFish <zhengyu_changhe@163.com>
2020-03-18 15:38:02 +0800

    关闭debug模式

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ul!bn9xi5_o2=e2*iooov#p8)1km@tuef_i#6*c&vt0w9oxnde'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','101.37.204.126']
 
